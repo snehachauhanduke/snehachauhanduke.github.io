74f3058fcdde05ca97042c6200239f4eaa1ece16
snehachauhanduke <snehachauhan.duke@gmail.com>
2026-01-18 20:13:53 -0500

    name changes for cover path

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,8 @@
 import './Projects.css'
 import humaCover from '/Users/sneha/Downloads/portfolio/src/assets/huma.AI cover.png'
 import EliCover from '/Users/sneha/Downloads/portfolio/src/assets/Eli Lily Cover.png'
-import EBNCover from '/Users/sneha/Downloads/portfolio/src/assets/EBNCover cover.png'
-import NetflixCover from '/Users/sneha/Downloads/portfolio/src/assets/Netflix Cover cover.png'
+import EBNCover from '/Users/sneha/Downloads/portfolio/src/assets/EBN Cover .png'
+import NetflixCover from '/Users/sneha/Downloads/portfolio/src/assets/Netflix Cover.png'
 
 
 interface Project {
